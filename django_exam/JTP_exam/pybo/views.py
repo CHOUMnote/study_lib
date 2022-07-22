ac2b14dec0665e0813d6818afab0c3ab94dd2502
CHOUMnote <rlawlgh1028@naver.com>
2022-07-23 05:20:21 +0900

    apply templates

--- django_exam/JTP_exam/pybo/views.py
+++ django_exam/JTP_exam/pybo/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
+from .models import Question
 
 class Root(View):
     def get(self, request):
         return redirect("/pybo/")
 
-def index(request):
-    return HttpResponse("dksadjflkjdsalkfjlkaf")
+def index(request):  
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+    #==> render(request, 템플릿, 템플릿으로 보낼 데이터(dict))
