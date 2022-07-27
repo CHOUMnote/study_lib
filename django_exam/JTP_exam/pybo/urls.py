8546713ea32d312204b7625d110687bd3bd537ac
CHOUMnote <rlawlgh1028@naver.com>
2022-07-27 16:27:22 +0900

    add name space

--- django_exam/JTP_exam/pybo/urls.py
+++ django_exam/JTP_exam/pybo/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
     path('', views.Root.as_view(), name="root"),
-    path('pybo/', views.index, name="home")
+    path('pybo/', views.index, name="index"),
+    path('pybo/<int:id>/', views.detail,name='detail')
 ]
--- django_exam/JTP_exam/pybo/views.py
+++ django_exam/JTP_exam/pybo/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
 from django.http import HttpResponse
 from .models import Question
 
 class Root(View):
     def get(self, request):
-        return redirect("/pybo/")
+        return redirect("pybo:index")
 
 def index(request):  
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    context = {'q_list': question_list}
     return render(request, 'pybo/question_list.html', context)
     #==> render(request, 템플릿, 템플릿으로 보낼 데이터(dict))
+
+def detail(request, id):
+    question = get_object_or_404(Question, pk=id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
