e35f24dd3a665931356fd8298b7a2465927d950c
CHOUMnote <rlawlgh1028@naver.com>
2022-08-02 10:55:23 +0900

    add name space + answer func +BUG FIX!

--- django_exam/JTJ_exam/pybo/urls.py
+++ django_exam/JTJ_exam/pybo/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
     path('', views.Root.as_view(), name="root"),
-    path('pybo/', views.index, name="home")
+    path('pybo/', views.index, name="home"),
+    path('pybo/<int:question_id>/', views.detail, name="detail"),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
