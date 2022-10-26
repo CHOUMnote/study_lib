99b9a13aa60ad29dc922493eadd390fa4a118bd4
CHOUMnote <rlawlgh1028@naver.com>
2022-10-26 16:55:48 +0900

    ip수정

--- django_exam/JTJ_exam/config/settings.py
+++ django_exam/JTJ_exam/config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-&mi!vg#(-ih_kbq#ydmikdr97&!=m_@i14(&-^az_l#dx+sq+#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["34.64.195.233"]
+ALLOWED_HOSTS = ["52.78.136.212"]
 
 
 # Application definition
